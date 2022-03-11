aed0e1d6b7f8a963e4767aee09bd75c4d706df1b
ChrisBob <98698407+ChrisBob1@users.noreply.github.com>
2022-03-11 11:30:40 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'bodacious'
+game 'gta5'
+ 
+files {
+	'contentunlocks.meta',
+	'loadouts.meta',
+	'pedpersonality.meta',
+	'shop_weapon.meta',
+	'weaponanimations.meta',
+	'weaponarchetypes.meta',
+	'weapons.meta'
+	}
+
+	data_file 'WEAPONINFO_FILE' 'weapons.meta'
+	data_file 'WEAPON_METADATA_FILE' 'weaponarchetypes.meta'
+	data_file 'WEAPON_SHOP_INFO' 'shop_weapon.meta'
+	data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
+	data_file 'CONTENT_UNLOCKING_META_FILE' 'contentunlocks.meta'
+	data_file 'LOADOUTS_FILE' 'loadouts.meta'
+	data_file 'PED_PERSONALITY_FILE' 'pedpersonality.meta'
